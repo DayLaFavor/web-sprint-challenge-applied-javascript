73de8fb8577904b8cf1c5199073e290d44ef2d6f
DayLaFavor <danelafavor1@gmail.com>
2021-10-29 12:30:49 -0500

    step 1

--- src/components/header.js
+++ src/components/header.js
@@ -1,6 +1,21 @@
 const Header = (title, date, temp) => {
   // TASK 1
-  // ---------------------
+  const header = document.createElement('div')
+  header.classList.add('header')
+
+  const date = document.createElement('span')
+  date.classList.add('date')
+
+  const title = document.createElement('h1')
+  title.classList.add('title')
+
+  const temp = document.createElement('span')
+  temp.classList.add('temp')
+
+  header.appendChild(date)
+  header.appendChild(title)
+  header.appendChild(temp)
+  
   // Implement this function taking `title`, `date` and `temp` as its 3 args and returning the markup below.
   // The tags used, the hierarchy of elements and their attributes must match the provided markup exactly!
   // The text inside elements will be set using their `textContent` property (NOT `innerText`).
